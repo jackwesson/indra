66e7abc2781ef2b469e8247eb17f9d183b4eb266
warrenbuhler <warrenbuhler7@gmail.com>
2016-03-08 19:23:44 +0000

    Initial Commit of Landing Page

--- splash/forms.py
+++ splash/forms.py
@@ -2,15 +2,9 @@ from django import forms
 
 
 # your_name = forms.CharField(label='Your name', max_length=100)
-class NameForm(forms.Form):
-    email = forms.EmailField(label='Your email', max_length=50)
-    # first = forms.CharField(label='First Name', max_length=50)
-    # last = forms.CharField(label='Last Name', max_length=50)
-    password = forms.CharField(label='Your password', max_length=50, widget=forms.PasswordInput)    
     
 class RegisterForm(forms.Form):
     email = forms.EmailField(label='Your Email', max_length=50)
-    first = forms.CharField(label='Your First Name', max_length=50)
-    last = forms.CharField(label='Your Last Name', max_length=50)
-    password = forms.CharField(label='Your Password', max_length=50, widget=forms.PasswordInput) 
-    password_confirmation = forms.CharField(label='Confirm Password', max_length=50, widget=forms.PasswordInput) 
+    name = forms.CharField(label='Your Name', max_length=50)
+    # password = forms.CharField(label='Your Password', max_length=50, widget=forms.PasswordInput) 
+    # password_confirmation = forms.CharField(label='Confirm Password', max_length=50, widget=forms.PasswordInput) 
--- splash/views.py
+++ splash/views.py
@@ -1,7 +1,6 @@
 #original 
 from django.shortcuts import render
 
-# from video https://www.youtube.com/watch?v=Txxa-NP1gXs&list=PLei96ZX_m9sXxvAlhV6El4HnpaClQOXa9&index=2
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django import template
@@ -9,72 +8,65 @@ from django.template.loader import get_template
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from  django.contrib.auth import authenticate, login as django_login, logout
-
-# working with forms
-from .forms import NameForm
 from .forms import RegisterForm
-
-#original
 from django.http import HttpResponse
 
-# Create your views here.
 
 
-#  https://www.youtube.com/watch?v=VtNHVuJq93U
-# potentially useful video series
 
 
-def splash(request):
-    # This displays the template of register and login
-    form = NameForm()
-    form2 = RegisterForm()
-
-    return render(request, 'name.html', {'form': form, 'form2': form2})
+def landing(request):
     
-def login(request):
-# This function allows the user to login
-    user = request.POST.get('email', '')
-    password = request.POST.get('password', '')
-    # authenticate is a django preset function
-    user = authenticate(username=user, password=password)
-    if user is not None:
-        if user.is_active:
-            django_login(request, user)
-            request.session['mid'] = user.id
-            # request.session['mid'] can be used to identify the user later
-            return HttpResponseRedirect ('/tasks/')
+    return render(request, 'index.html')
+    
+    
+def thanks(request):
+    email = request.POST['email']
+    if email != "":
+        u = User.objects.create_user(username=email, email=email)
+        u.save()
+        return render(request, 'thanks.html')
     else:
+        
+        return render(request, 'index.html')
+# def login(request):
+# # This function allows the user to login
+#     user = request.POST.get('email', '')
+#     password = request.POST.get('password', '')
+#     # authenticate is a django preset function
+#     user = authenticate(username=user, password=password)
+#     if user is not None:
+#         if user.is_active:
+#             django_login(request, user)
+#             request.session['mid'] = user.id
+#             # request.session['mid'] can be used to identify the user later
+#             return HttpResponseRedirect ('/tasks/')
+#     else:
        
-        return HttpResponse("Sorry your login information was wrong")
+#         return HttpResponse("Sorry your login information was wrong")
     
 
 def register(request):
-    if (request.POST.get('password', '') == request.POST.get('password_confirmation', '')):
-        email = request.POST.get('email', '')
-        fname = request.POST.get('first', '')
-        lname = request.POST.get('last', '')
-        password = request.POST.get('password', '')
+    # if (request.POST.get('password', '') == request.POST.get('password_confirmation', '')):
+    email = request.POST.get('email', '')
+    name = request.POST.get('name', '')
+        # password = request.POST.get('password', '')
         
         
         # The user model is a preset django model
-        try:
-            user = User.objects.create_user(username=email, password=password, email=email, first_name=fname, last_name=lname)
-            user.save()
-            user = authenticate(username=email, password=password)
-            if user is not None:
-                if user.is_active:
-                    django_login(request, user)
-                    # return HttpResponse("You are now logged in")
-                    request.session['mid'] = user.id
-                    return HttpResponseRedirect ('/tasks/')
-            
-        except:
-            return HttpResponse("Sorry please try again with different inputs")
+    try:
+        user = User.objects.create_user(username=email, email=email, first_name=name)
+        user.save()
         
-    else:
-        # print request.POST.get('password', '')
-        # print request.POST.get('password_confirmation', '')
-        return HttpResponse("your password and confirmation did not match")
+        return HttpResponseRedirect ('/')
+        
+    except:
+        return HttpResponse("Sorry please try again with different inputs")
+        
+    # else:
+    #     # print request.POST.get('password', '')
+    #     # print request.POST.get('password_confirmation', '')
+    #     return HttpResponse("your password and confirmation did not match")
     
     # return render(request, 'djangosocialtwodo/splash.html')
     # return render_to_response('djangosocialtwodo/splash.html', {}, RequestContext(request))
