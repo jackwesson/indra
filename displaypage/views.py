c92c5cde906ca15b8aab4688491630e80cab46c8
warrenbuhler <warrenbuhler7@gmail.com>
2016-04-21 00:53:12 +0000

    fixed problem with concatenating urls

--- displaypage/views.py
+++ displaypage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+
 from django.template import RequestContext
 from django import template
 from django.template.loader import get_template 
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from  django.contrib.auth import authenticate, login as django_login, logout as pooplogout
 from django.db.models import Q
-
+from django.shortcuts import render_to_response, redirect
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -24,6 +24,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        return redirect('/displaypage')
     artists = User.objects.all()
     content = artists
     
